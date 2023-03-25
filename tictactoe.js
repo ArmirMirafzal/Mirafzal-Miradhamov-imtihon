93d6b0230162b0944d4606875a4aacc3e99154ac
Armir <mirarmirkhan@gmail.com>
2023-03-25 14:09:55 +0500

    first commit

---
+++ tictactoe.js
@@ -0,0 +1,178 @@
+"use strict";
+let board;
+let playerO = "O";
+let playerX = "X";
+let currPlayer = playerX;
+let gameOver = false;
+let nextRaund = document.querySelector(".next");
+let Xscore = document.querySelector(".Xscore");
+let Oscore = document.querySelector(".Oscore");
+let raundcounter = document.querySelector(".raund");
+let number,
+  gaming,
+  selected,
+  xScore,
+  oScore,
+  roundCount,
+  roundEnd,
+  winnerPlayer,
+  endAllRounds;
+
+window.onload = function () {
+  setGame();
+};
+
+function setGame() {
+  board = [
+    [" ", " ", " "],
+    [" ", " ", " "],
+    [" ", " ", " "],
+  ];
+  for (let r = 0; r < 3; r++) {
+    for (let c = 0; c < 3; c++) {
+      let tile = document.createElement("div");
+      tile.id = r.toString() + "-" + c.toString();
+      tile.classList.add("tile");
+      if (r == 0 || r == 1) {
+        tile.classList.add("horizontal-line");
+      }
+      if (c == 0 || c == 1) {
+        tile.classList.add("vertical-line");
+      }
+      tile.innerText = "";
+      tile.addEventListener("click", setTile);
+      document.getElementById("board").appendChild(tile);
+    }
+  }
+}
+
+nextRaund.addEventListener("click", nextRoundGame);
+
+function setTile() {
+  if (gameOver) {
+    return;
+  }
+
+  let coords = this.id.split("-"); //ex) "1-2" -> ["1", "2"]
+  let r = parseInt(coords[0]);
+  let c = parseInt(coords[1]);
+
+  if (board[r][c] != " ") {
+    return;
+  }
+
+  board[r][c] = currPlayer;
+  this.innerText = currPlayer;
+
+  //change players
+  if (currPlayer == playerX) {
+    currPlayer = playerO;
+  } else {
+    currPlayer = playerX;
+  }
+  checkWinner();
+  davrlar();
+}
+
+function checkWinner() {
+  xScore = 0;
+  oScore = 0;
+  //horizontally, check 3 rows
+  for (let r = 0; r < 3; r++) {
+    if (
+      board[r][0] == board[r][1] &&
+      board[r][1] == board[r][2] &&
+      board[r][0] != " "
+    ) {
+      for (let i = 0; i < 3; i++) {
+        let tile = document.getElementById(r.toString() + "-" + i.toString());
+        tile.classList.add("winner");
+      }
+      // if (playerX) {
+      //   xScore += 1;
+      //   Xscore.textContent = `X - ${xScore}`;
+      // } else {
+      //   oScore += 1;
+      //   Oscore.textContent = `O - ${oScore}`;
+      // }
+      gameOver = true;
+      roundEnd = true;
+      return;
+    }
+  }
+  //vertically, check 3 columns
+  for (let c = 0; c < 3; c++) {
+    if (
+      board[0][c] == board[1][c] &&
+      board[1][c] == board[2][c] &&
+      board[0][c] != " "
+    ) {
+      //if we found the winning col
+      for (let i = 0; i < 3; i++) {
+        let tile = document.getElementById(i.toString() + "-" + c.toString());
+        tile.classList.add("winner");
+      }
+      gameOver = true;
+      roundEnd = true;
+
+      return;
+    }
+  }
+  //diagonally
+  if (
+    board[0][0] == board[1][1] &&
+    board[1][1] == board[2][2] &&
+    board[0][0] != " "
+  ) {
+    for (let i = 0; i < 3; i++) {
+      let tile = document.getElementById(i.toString() + "-" + i.toString());
+      tile.classList.add("winner");
+    }
+  
+    gameOver = true;
+    roundEnd = true;
+
+    return;
+  }
+  //anti-diagonally
+  if (
+    board[0][2] == board[1][1] &&
+    board[1][1] == board[2][0] &&
+    board[0][2] != " "
+  ) {
+    //0-2
+    let tile = document.getElementById("0-2");
+    tile.classList.add("winner");
+    //1-1
+    tile = document.getElementById("1-1");
+    tile.classList.add("winner");
+    //2-0
+    tile = document.getElementById("2-0");
+    tile.classList.add("winner");
+    gameOver = true;
+    roundEnd = true;
+
+    return;
+  }
+}
+
+/* */
+
+function clear() {
+  selected = false;
+  roundEnd = false;
+  gaming = true;
+  endAllRounds = false;
+}
+
+
+
+function davrlar(){  
+  davr = board[r][c];
+  const list  = document.createElement('li');
+  const btn  = document.createElement('button');
+
+  btn.textContent = 'Go to game start';
+  btn.classList.add('davr');
+  list.appendChild(btn);
+}
